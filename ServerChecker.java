0d30d0950104fa9e16f757b3fadc524858d12866
Martin Kristensen <mail@azgul.com>
2013-10-20 18:21:07 +0200

    OSX fix

--- ServerChecker.java
+++ ServerChecker.java
@@ -99,7 +99,7 @@ public class ServerChecker {
 				// nasty as hell, I know ....
 				String[] arr = strContents.split("  ");
 				String ip = arr[arr.length-1];
-				if (ip.length() >= 2) {
+				if (ip.split(" ").length >= 2) {
 					ip = ip.split(" ")[1];
 					ip = ip.replace(".c.bnetg", "");
 					ip = ip.replace(".cu.bnetg", "");
